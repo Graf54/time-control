bda31477b370aa49a8f6572dd60a11d13ee0b6d4
IvanDev <IvanDeveloper84@ya.ru>
2019-07-09 02:34:22 +0700

    delete spring mvc

--- src/main/java/home/App.kt
+++ src/main/java/home/App.kt
@@ -2,11 +2,9 @@ package home
 
 import home.core.impl.Core
 import home.db.dao.DAOConfig
-import home.web.launcher.Launcher
 
 fun main() {
     DAOConfig.init()
-    Launcher.start(12120)
     val core = Core(1)
     core.start()
 }
